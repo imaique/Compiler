e064cee26762837bb3100f0512c6bb0b6f7024af
Michael <osujim2012@gmail.com>
2023-01-23 20:07:40 -0500

    remove consume_char and return responses instead of state pointers

--- LexicalAnalyzer/State/State.h
+++ LexicalAnalyzer/State/State.h
@@ -1,18 +1,57 @@
 #pragma once
+#include <iostream>
+#include <unordered_map>
+#include "../Token.h"
+
+struct Response {
+public:
+	Response(bool, State*);
+	const bool consume;
+	const State* nextState;
+};
 
 class State {
-	static State* initialState;
-	bool is_final_state;
+protected:
+	static State* initial_state;
+	static State* error_state;
+	State* unsupported_character_transition;
+	const bool is_final_state = false;
 
-	State(bool);
-
-	virtual State* get_next_state(char);
-	virtual bool consume_char(char);
+	virtual Response get_next_state(char) = 0;
 };
 
 class LetterState: virtual public State {
 	State* letter_transition;
 public:
-	State* get_next_state(char);
-	bool consume_char(char);
+	LetterState(State*);
+	Response get_next_state(char);
+};
+
+class DigitState : virtual public State {
+	State* digit_transition;
+public:
+	DigitState(State*);
+	Response get_next_state(char);
+};
+
+class FinalState : virtual public State {
+	const bool is_final_state = true;
+	const Token::Type token_type;
+public:
+	Response get_next_state(char);
+	Token::Type get_token_type();
+};
+
+class BlockCommentState : virtual public State {
+	State* after_comment_transition;
+	int counter = 1;
+	char previous_character = 'a';
+public:
+	Response get_next_state(char);
+};
+
+class CharacterState : virtual public State {
+	const std::unordered_map<char, State*> valid_transitions;
+public:
+	Response get_next_state(char);
 };
