bf180dde86960d1abd1c80dcf64c816436ff9c6c
Michael <osujim2012@gmail.com>
2023-01-26 19:52:01 -0500

    Test LexicalAnalyzer

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
+
+#include "LexicalAnalyzer/LexicalAnalyzer.h"
 
 int main() {
-	std::cout << "Hello";
+	
+	LexicalAnalyzer LA = LexicalAnalyzer("file.txt");
+	std::cout << LA.has_next_token() << std::endl;
+	Token token = LA.get_next_token();
+	std::cout << token;
 	std::cin.get();
+	
 }
