91e9754e71a1f5440611250aa83a04b76522f044
Michael <osujim2012@gmail.com>
2023-03-02 12:10:31 -0500

    TEST MODE

--- ASTdriver.cpp
+++ ASTdriver.cpp
@@ -15,39 +15,47 @@ void traverse_ast(AST* node, int& unique, std::ofstream& dot_file) {
 
     dot_file << "\"];" << endl;
 
-    for (AST* child_node : node->children) {
+    for (int i = node->children.size() - 1; i >= 0; i--) {
+        AST* child_node = node->children[i];
         dot_file << node_id << "->" << unique << ";";
         traverse_ast(child_node, unique, dot_file);
     }
 }
 
-void write_ast(AST* root, string filename) {
-    if (!root) return;
+void write_ast(AST* root, string filename, std::stack<AST*> m_stack) {
+    
     std::ofstream dot_file("output/outAST/" + filename + ".dot.outast");
     dot_file << "digraph AST {" << endl;
     dot_file << "node [shape=record];" << endl;
     dot_file << "node [fontname=Sans];charset=\"UTF - 8\" splines=true splines=spline rankdir =LR" << endl;
 
     int unique = 1;
-    traverse_ast(root, unique, dot_file);
+
+    if (root) {
+        m_stack.push(root);
+        while (!m_stack.empty()) {
+            traverse_ast(m_stack.top(), unique, dot_file);
+            m_stack.pop();
+        }
+    }
 
     dot_file << "}";
 }
 
 int main() {
-    std::vector<std::string> test_files{ "example-polynomial", "example-bubblesort" };
+    std::vector<std::string> test_files{ "syntax-func", }; //"example-polynomial", "example-bubblesort" };
 
     for (auto filename : test_files) {
         std::string source_file = "input/" + filename + ".src";
         if (std::filesystem::exists(source_file)) {
             SemanticAnalyzer sa = SemanticAnalyzer(filename);
             AST* root = sa.get_AST();
-            write_ast(root, filename);
+            std::stack<AST*> m_stack = sa.m_stack;
+            write_ast(root, filename, m_stack);
         }
         else {
             std::cout << source_file << " doesn't exist. Please make sure the file is in the input folder. Skipping file." << std::endl;
         }
     }
-    std::cin.get();
 }
 
