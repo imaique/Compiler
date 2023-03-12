b878a1e97c964375bf4be39bb132ba1e589d379c
Michael <osujim2012@gmail.com>
2023-03-12 19:28:13 -0400

    add error logic and complete function generation and construction of symbol tables

--- Semantic/SemanticAnalyzer.cpp
+++ Semantic/SemanticAnalyzer.cpp
@@ -1,5 +1,6 @@
 #include "SemanticAnalyzer.h"
 #include "ASTGenerator.h"
+#include <algorithm>
 
 
 
@@ -8,12 +9,22 @@ typedef SymbolTableEntry STE;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::to_string;
 typedef SymbolTableEntry::Kind EntryKind;
 
 using namespace ASTConstants;
 
 SemanticAnalyzer::SemanticAnalyzer(string filename) : filename(filename) {
 
+}
+
+SemanticAnalyzer::SemanticError::SemanticError(std::string text, int line_location) : text(text), line_location(line_location) {
+
+}
+
+void SemanticAnalyzer::add_error(std::string text, int line) {
+	SemanticError error(text, line);
+	errors.push_back(error);
 }
 
 bool SemanticAnalyzer::analyze() {
@@ -26,7 +37,21 @@ bool SemanticAnalyzer::analyze() {
 	SymbolTable* global_table = construct_symbol_tables(root);
 	std::ofstream file("output/outsemantic/" + filename + ".outsymboltables");
 	file << *global_table;
+
+	print_errors();
 	return true;
+}
+
+bool operator<(const SemanticAnalyzer::SemanticError& e1, const SemanticAnalyzer::SemanticError& e2)
+{
+	return e1.line_location < e2.line_location;
+}
+
+void SemanticAnalyzer::print_errors() {
+	std::ofstream file("output/outsemantic/" + filename + ".outsemanticerrors");
+	std::sort(errors.begin(), errors.end());
+
+	for (SemanticError error : errors) file << error.text << " (line " << error.line_location  << ")" << endl;
 }
 
 
@@ -65,17 +90,29 @@ SymbolTableEntry* SemanticAnalyzer::generate_class_entry(AST* class_node) {
 		SymbolTableEntry* duplicate_entry = class_table->add_entry_if_new(entry);
 
 		if (entry != duplicate_entry) {
-
+			add_error("There already exists a variable of the name " + entry->name + " in class " + name + " at line " + to_string(duplicate_entry->line_location) + ".", line_location);
 		}
 	}
 
 	const vector<AST*> functions = get_types(children, FuncDecl);
 
+	for (AST* func_decl : functions) {
+		SymbolTableEntry* entry = generate_function_entry(func_decl, STE::Kind::FuncDecl, name);
+
+		SymbolTableEntry* duplicate_entry = class_table->add_entry_if_new(entry);
+
+		if (entry != duplicate_entry) {
+			add_error("There already exists a method of the name " + entry->unique_id + " in class " + name + " at line " + to_string(duplicate_entry->line_location) + ".", line_location);
+		}
+	}
+
 
 	return new SymbolTableEntry(name, name, STE::Kind::Class, nullptr, line_location, STE::Visibility::None, class_table);
 }
 
-SymbolTableEntry* SemanticAnalyzer::generate_function_entry(AST* func_node) {
+SymbolTableEntry* SemanticAnalyzer::generate_function_entry(AST* func_node, STE::Kind func_kind, string parent_class) {
+	if (func_kind != STE::Kind::FuncDecl && func_kind != STE::Kind::FuncDef) return nullptr;
+
 	const vector<AST*>& children = func_node->children;
 
 	const AST* id = get_type(children, Id);
@@ -86,14 +123,75 @@ SymbolTableEntry* SemanticAnalyzer::generate_function_entry(AST* func_node) {
 
 	const string name = id->value;
 
+	SymbolTable* func_table = new SymbolTable(name);
+
 	const AST* type_node = get_type(children, Type);
 
-	const string type_id = type_node->value;
+	const string type_id = type_node ? type_node->value : parent_class;
 
-	return nullptr;
+	const AST* param_list = get_type(children, ParamList);
+
+	vector<SymbolType> parameters;
+
+	for (AST* param : param_list->children) {
+		SymbolTableEntry* entry = generate_variable_entry(param, STE::Kind::Parameter);
+
+		SymbolTableEntry* duplicate_entry = func_table->add_entry_if_new(entry);
+
+		if (entry != duplicate_entry) {
+			add_error("There already exists a parameter of the name " + entry->name + " at line " + to_string(duplicate_entry->line_location) + ".", line_location);
+		}
+		else {
+			parameters.push_back(*entry->type);
+		}
+	}
+
+
+	FunctionSymbolType* type = new FunctionSymbolType(type_id, parameters);
+
+	if (func_kind == STE::Kind::FuncDef) {
+		const AST* stat_block = get_type(children, StatBlock);
+		const vector<AST*> localvars = get_types(stat_block->children, VarDecl);
+		for (AST* localvar : localvars) {
+			SymbolTableEntry* entry = generate_variable_entry(localvar, STE::Kind::Localvar);
+
+			SymbolTableEntry* duplicate_entry = func_table->add_entry_if_new(entry);
+
+			if (entry != duplicate_entry) {
+				add_error("There already exists a variable or parameter of the name " + entry->name + " at line " + to_string(duplicate_entry->line_location) + ".", line_location);
+			}
+		}
+	}
+
+	string unique_id = name + type->signature;
+
+	
+	const AST* visibility_node = get_type(children, Visibility);
+
+	STE::Visibility visibility = get_visibility(visibility_node);
+
+	return new SymbolTableEntry(unique_id, name, func_kind, type, line_location, visibility, func_table);
+}
+
+STE::Visibility SemanticAnalyzer::get_visibility(const AST* visibility_node) {
+	STE::Visibility visibility = STE::Visibility::None;
+
+	if (visibility_node) {
+		if (visibility_node->value == "private") {
+			visibility = STE::Visibility::Private;
+		}
+		else {
+			visibility = STE::Visibility::Public;
+		}
+	}
+
+	return visibility;
 }
 
 SymbolTableEntry* SemanticAnalyzer::generate_variable_entry(AST* var_node, EntryKind kind) {
+	if (kind == EntryKind::Localvar) {
+		cout << "ayo";
+	}
 	const vector<AST*>& children = var_node->children;
 
 	const AST* id = get_type(children, Id);
@@ -110,22 +208,14 @@ SymbolTableEntry* SemanticAnalyzer::generate_variable_entry(AST* var_node, Entry
 
 	const AST* dim_list = get_type(children, DimList);
 
-	vector<int> dimensions = get_dimensions(dim_list->children);
+	vector<int> dimensions;
+	if(dim_list) dimensions = get_dimensions(dim_list->children);
 
 	SymbolType* type = new SymbolType(type_id, dimensions);
 
 	const AST* visibility_node = get_type(children, Visibility);
 
-	STE::Visibility visibility = STE::Visibility::None;
-
-	if (visibility_node) {
-		if (visibility_node->value == "private") {
-			visibility = STE::Visibility::Private;
-		}
-		else {
-			visibility = STE::Visibility::Public;
-		}
-	}
+	STE::Visibility visibility = get_visibility(visibility_node);
 
 	return new SymbolTableEntry(name, name, kind, type, line_location, visibility, nullptr);
 }
@@ -133,8 +223,12 @@ SymbolTableEntry* SemanticAnalyzer::generate_variable_entry(AST* var_node, Entry
 vector<int> SemanticAnalyzer::get_dimensions(const std::vector <AST*> dimension_nodes) {
 	vector<int> dimensions;
 
-	for (AST* dimension : dimension_nodes) 
-		dimensions.push_back(stoi(dimension->value));
+	for (AST* dimension : dimension_nodes) {
+		int value = -1;
+		if (dimension->value.size()) value = stoi(dimension->value);
+		dimensions.push_back(value);
+	}
+		
 
 	return dimensions;
 }
@@ -158,6 +252,59 @@ SymbolTable* SemanticAnalyzer::construct_symbol_tables(AST* node) {
 		// there's already a class of this name
 		if (duplicate_entry != entry) {
 			cout << "The class " << entry->name << " declared at line " << entry->line_location << " has already been declared at line " << duplicate_entry->line_location << "." << endl;
+		}
+	}
+
+	for (AST* func_def : func_defs) {
+		AST* scope = get_type(func_def->children, Scope);
+		AST* scope_id = get_type(scope->children, Id);
+
+		if (scope_id) {
+			
+			string class_name = scope_id->value;
+
+
+			SymbolTableEntry* class_entry = global_table->get_entry(class_name);
+			if (class_entry) {
+				SymbolTableEntry* entry = generate_function_entry(func_def, STE::Kind::FuncDef, class_name);
+
+				SymbolTable* class_symbol_table = class_entry->link;
+
+				// if nullptr, no DECLARATION
+				// else 
+					// if funcdecl, then override
+					// else already defined
+				SymbolTableEntry* current_entry = class_symbol_table->get_entry(entry->unique_id);
+
+				if (current_entry) {
+					if (current_entry->kind == STE::Kind::FuncDecl) {
+						current_entry->kind = entry->kind;
+						current_entry->line_location = entry->line_location;
+
+						delete current_entry->link;
+
+						current_entry->link = entry->link;
+					}
+					else {
+						add_error("The function " + entry->unique_id + " is already defined in class " + class_name + ".", entry->line_location);
+					}
+				}
+				else {
+					add_error("No function declaration for " + entry->unique_id + " in class " + class_name + ".", entry->line_location);
+				}
+			}
+			else {
+				add_error("The class " + class_name + " does not exist.", scope_id->token->line_location);
+			}
+		}
+		else {
+			SymbolTableEntry* entry = generate_function_entry(func_def, STE::Kind::FuncDef, "error");
+
+			SymbolTableEntry* duplicate_entry = global_table->add_entry_if_new(entry);
+
+			if (entry != duplicate_entry) {
+				add_error("There already exists a function " + entry->unique_id + " at line " + to_string(duplicate_entry->line_location) + ".", entry->line_location);
+			}
 		}
 	}
 
--- Semantic/SemanticAnalyzer.h
+++ Semantic/SemanticAnalyzer.h
@@ -5,17 +5,31 @@
 
 class SemanticAnalyzer {
 private:
+	class SemanticError {
+	public:
+		int line_location;
+		std::string text;
+
+		SemanticError(std::string text, int line_location);
+	};
+	
+	std::vector<SemanticError> errors;
+	void add_error(std::string text, int line);
+	void print_errors();
 	std::string filename;
 	SymbolTable* construct_symbol_tables(AST* node);
 
 	//SymbolTableEntry* generate_entry(AST* node, SymbolTableEntry::Kind kind);
 	SymbolTableEntry* generate_class_entry(AST* class_node);
-	SymbolTableEntry* generate_function_entry(AST* function_node);
+	SymbolTableEntry* generate_function_entry(AST* function_node, SymbolTableEntry::Kind func_kind, std::string parent_class);
 	SymbolTableEntry* generate_variable_entry(AST* var_node, SymbolTableEntry::Kind kind);
 	std::vector<int> get_dimensions(const std::vector <AST*> dimension_nodes);
 
+	SymbolTableEntry::Visibility SemanticAnalyzer::get_visibility(const AST* visibility_node);
 	std::vector<AST*>  get_types(const std::vector <AST*>& list, std::string type);
 	AST* get_type(const std::vector <AST*>& list, std::string type);
+
+	friend bool operator<(const SemanticAnalyzer::SemanticError& p1, const SemanticAnalyzer::SemanticError& p2);
 
 public:
 
