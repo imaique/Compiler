ab0076cab06156fe86d0923da332cca49175de24
Michael <osujim2012@gmail.com>
2023-03-12 14:32:36 -0400

    Implement symbol tables

--- Semantic/SymbolTable.cpp
+++ Semantic/SymbolTable.cpp
@@ -1,15 +1,165 @@
 #include "SymbolTable.h"
+#include <sstream>
+#include <iostream>
 
 using std::string;
+typedef SymbolTableEntry STE;
+using std::vector;
+using std::endl;
 
+const std::unordered_map<STE::Kind, std::string> SymbolTableEntry::kind_strings{
+		{Kind::Class, "class"},
+		{Kind::Function, "function"},
+		{Kind::Parameter, "param"},
+		{Kind::Inherit, "inherit"},
+		{Kind::Data, "data"},
+		{Kind::Localvar, "local"},
+};
+
+const std::unordered_map<STE::Visibility, std::string> SymbolTableEntry::visibility_strings{
+		{Visibility::Public, "public"},
+		{Visibility::Private, "private"},
+};
+
+
+// SymbolType
+
+std::ostream& operator<<(std::ostream& os, const SymbolType& type) {
+	os << type.type_id;
+	for (int dimension : type.dimensions) {
+		os << "[" << dimension << "]";
+	}
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const FunctionSymbolType& type) {
+	os << type.type_id;
+	for (int dimension : type.dimensions) {
+		os << "[" << dimension << "]";
+	}
+
+	os << " : ";
+
+	bool first = true;
+
+	for (SymbolType st : type.param_types) {
+		if (!first) {
+			os << ", ";
+		}
+		else {
+			first = false;
+		}
+		os << st;
+	}
+	return os;
+}
+
+// SymbolTable
+
+SymbolType::SymbolType(std::string type_id, std::vector<int> dimensions) : type_id(type_id), dimensions(dimensions) {
+
+}
+
+SymbolTable::SymbolTable(std::string name) : name(name) {
+
+}
+
+void SymbolTable::add_entry(SymbolTableEntry* entry) {
+	entries.insert({entry->unique_id, entry});
+}
+
+std::string FunctionSymbolType::get_signature(std::vector<SymbolType> param_types) {
+	std::stringstream ss;
+	ss << '(';
+	bool first = true;
+	for (SymbolType param_type : param_types) {
+		if (!first) {
+			ss << ", ";
+		}
+		else {
+			first = false;
+		}
+		ss << param_type;
+	}
+	ss << ')';
+	return ss.str();
+}
+
+FunctionSymbolType::FunctionSymbolType(std::string return_type, std::vector<int> return_dimensions, std::vector<SymbolType> param_types) 
+	: SymbolType(return_type, return_dimensions), param_types(param_types), signature(get_signature(param_types)) {
+
+}
+
+vector<vector<int>> SymbolTable::get_spaces() const {
+	vector<vector<int>> widths;
+	for (const auto& entry : entries) {
+
+	}
+	return widths;
+}
+
+void SymbolTable::print_table(std::ostream& os, std::string prefix) const {
+	os << prefix << "table: " << name << endl;
+	std::cout << entries.size();
+	for (const auto& pair : entries) {
+		SymbolTableEntry* entry = pair.second;
+		std::string kind_string = SymbolTableEntry::kind_strings.at(entry->kind);
+		os << prefix << kind_string << "\t| " << entry->name;
+
+		SymbolType* type = entry->type;
+		STE::Visibility visibility = entry->visibility;
+
+		if (type) {
+			os << "\t| " << *type;
+		}
+		if (visibility != STE::Visibility::None) {
+			os << "\t| " << STE::visibility_strings.at(visibility);
+		}
+		os << endl;
+
+		if (entry->link) entry->link->print_table(os, prefix + '\t');
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const SymbolTable& table) {
+	//vector<vector<int>> spaces = table.get_spaces();
+	table.print_table(os, "");
+	//os << table.name;
+	return os;
+}
+
+
+// SymbolTableEntry
+
+/*
 SymbolTableEntry::SymbolTableEntry(std::string name, Kind kind, std::string type) :
 	name(name), kind(kind), type(type), link(nullptr), nextEntry(nullptr)
 {
 
 }
+*/
 
-SymbolTableEntry::SymbolTableEntry(std::string name, Kind kind, std::string type, SymbolTable* link) :
-	name(name), kind(kind), type(type), link(link), nextEntry(nullptr)
+SymbolTableEntry::SymbolTableEntry(std::string unique_id, std::string name, Kind kind, SymbolType* type, int line_location, Visibility visibility, SymbolTable* link) :
+	unique_id(unique_id), name(name), kind(kind), type(type), link(link), line_location(line_location), nextEntry(nullptr), visibility(visibility)
 {
 
+}
+
+SymbolTableEntry* SymbolTable::get_entry(std::string unique_id, SymbolTableEntry::Kind kind) {
+	if (!entries.count(unique_id)) return nullptr;
+
+	SymbolTableEntry* entry = entries.at(unique_id);
+
+	while (entry && entry->kind != kind) entry = entry->nextEntry;
+
+	return entry;
+}
+
+SymbolTableEntry* SymbolTable::add_entry_if_new(SymbolTableEntry* entry) {
+	SymbolTableEntry* duplicate_entry = this->get_entry(entry->unique_id, STE::Kind::Class);
+
+	if (duplicate_entry) return duplicate_entry;
+
+	this->add_entry(entry);
+	return entry;
 }
--- Semantic/SymbolTable.h
+++ Semantic/SymbolTable.h
@@ -1,18 +1,34 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include <fstream>
 
-class SymbolTable {
-	
-	std::unordered_map<std::string, SymbolTableEntry*> entries;
+class SymbolTable;
+
+class SymbolType {
 
 public:
-	SymbolTableEntry* get_entry(std::string name, SymbolTableEntry::Kind kind);
-	bool add_entry(SymbolTableEntry* entry);
+	const std::string type_id;
+	const std::vector<int> dimensions;
+	SymbolType(std::string type_id, std::vector<int> dimensions);
 
-	const std::string name;
+	friend std::ostream& operator<<(std::ostream& os, const SymbolType& type);
+
+	virtual ~SymbolType() = default;
 };
 
+class FunctionSymbolType : SymbolType {
+	std::string get_signature(std::vector<SymbolType> param_types);
+public:
+	std::vector<SymbolType> param_types;
+	const std::string signature;
+
+	FunctionSymbolType(std::string return_type, std::vector<int> return_dimensions, std::vector<SymbolType> param_types);
+
+	friend std::ostream& operator<<(std::ostream& os, const FunctionSymbolType& type);
+	
+	//std::string get_signature();
+};
 
 
 class SymbolTableEntry {
@@ -21,15 +37,53 @@ public:
 		Function,
 		Class,
 		Parameter,
-		Variable,
+		Data,
+		Localvar,
+		Inherit
 	};
+	static const std::unordered_map<Kind, std::string> kind_strings;
+	enum class Visibility {
+		None,
+		Public,
+		Private
+	};
+	static const std::unordered_map<Visibility, std::string> visibility_strings;
+	std::string unique_id;
 	std::string name;
 	Kind kind;
-	std::string type;
+	SymbolType* type;
+	Visibility visibility;
+	int line_location;
 	SymbolTable* link;
 	// In case of name collision
 	SymbolTableEntry* nextEntry;
 
-	SymbolTableEntry(std::string name, Kind kind, std::string type);
-	SymbolTableEntry(std::string name, Kind kind, std::string type, SymbolTable* link);
+	//SymbolTableEntry(std::string name, Kind kind, std::string type);
+	SymbolTableEntry(std::string unique_id, std::string name, Kind kind, SymbolType* type, int line_location, Visibility visibility, SymbolTable* link);
+
+};
+
+class SymbolTable {
+	
+	std::unordered_map<std::string, SymbolTableEntry*> entries;
+	std::vector<std::vector<int>> get_spaces() const;
+	void print_table(std::ostream& os, std::string prefix) const;
+
+public:
+	SymbolTableEntry* get_entry(std::string name, SymbolTableEntry::Kind kind);
+	SymbolTableEntry* add_entry_if_new(SymbolTableEntry* entry);
+	void add_entry(SymbolTableEntry* entry);
+
+	const std::string name;
+	SymbolTable(std::string name);
+
+	friend std::ostream& operator<<(std::ostream& os, const SymbolTable& table);
+};
+
+
+
+
+
+class SymbolTableClassEntry : SymbolTableEntry {
+	std::unordered_map <std::string, std::string> inherited_members;
 };
