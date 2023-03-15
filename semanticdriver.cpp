0e1c0dbedd2a7317e41c83e8dd0fdc8116592067
Michael <osujim2012@gmail.com>
2023-03-14 22:22:48 -0400

    Update semanticdriver.cpp

--- semanticdriver.cpp
+++ semanticdriver.cpp
@@ -4,10 +4,11 @@
 #include <filesystem>
 
 int main() {
-    std::vector<std::string> test_files{ "syntax-func" };//, "example-polynomial", "example-bubblesort", "full-coverage" };
+    std::vector<std::string> test_files{ "syntax-func", "example-polynomial", "example-bubblesort" };//, "full-coverage" };
 
     for (auto filename : test_files) {
         std::string source_file = "input/" + filename + ".src";
+        std::cout << filename << std::endl;
         if (std::filesystem::exists(source_file)) {
             SemanticAnalyzer sa = SemanticAnalyzer(filename);
             sa.analyze();
