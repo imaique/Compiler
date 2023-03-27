5cd531c6762643498cad74dbeab34473159217c3
Michael <osujim2012@gmail.com>
2023-03-27 14:29:49 -0400

    CHECKPOINT

---
+++ CodeGeneration/CodeGenerator.cpp
@@ -0,0 +1,11 @@
+#include "CodeGenerator.h"
+
+using namespace std;
+
+CodeGenerator::CodeGenerator(string filename) : moon_file("output/outcode/" + filename + ".moon") {
+
+}
+
+void CodeGenerator::generate(AST* root, SymbolTable* global_table) {
+
+}
--- CodeGeneration/CodeGenerator.h
+++ CodeGeneration/CodeGenerator.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "../Semantic/AST.h"
+class CodeGenerator {
+private:
+	std::ofstream moon_file;
+public:
+	CodeGenerator(std::string filename);
+	void generate(AST* root, SymbolTable* global_table);
+};
