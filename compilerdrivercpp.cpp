24b158d5e7958164477d96501ea66bf340fa759c
Michael <osujim2012@gmail.com>
2023-04-02 21:36:30 -0400

    add compilerdriver

---
+++ compilerdrivercpp.cpp
@@ -0,0 +1,22 @@
+#include "Semantic/SemanticAnalyzer.h"
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+int main() {
+    std::vector<std::string> test_files{ "syntax-func", "example-bubblesort" }; //"polynomialsemanticerrors", "example-polynomial", ,
+//, "full-coverage" };
+
+    for (auto filename : test_files) {
+        std::string source_file = "input/" + filename + ".src";
+        std::cout << filename << std::endl;
+        if (std::filesystem::exists(source_file)) {
+            SemanticAnalyzer sa = SemanticAnalyzer(filename);
+            sa.analyze();
+        }
+        else {
+            std::cout << source_file << " doesn't exist. Please make sure the file is in the input folder. Skipping file." << std::endl;
+        }
+    }
+    //std::cin.get();
+}
