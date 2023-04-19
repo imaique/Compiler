1d748379714935c940e2ae5c50616e800bf1dacb
Michael <osujim2012@gmail.com>
2023-04-19 12:48:20 -0400

    Update compilerdrivercpp.cpp

--- compilerdrivercpp.cpp
+++ compilerdrivercpp.cpp
@@ -13,10 +13,12 @@ int main() {
     Compiler compiler;
 
     while (true) {
-        cout << "Please enter the name of the file (without the extension) you want to compile or type \"exit\" to stop compiling files. The avaiables files in the input directory are:" << endl;
+        cout << "Please enter the name of the file (without the extension) you want to compile, " << 
+            "type \"exit\" to stop compiling files or type \"all\" to compile all files in the directory. The avaiables files in the input directory are:" << endl;
         string directory_path = "input";
         for (const auto& file : directory_iterator(directory_path)) {
-            cout << file.path().filename() << endl;
+            string filename = file.path().stem().string();
+            cout << "- " << filename << endl;
         }
         string filename;
         cin >> filename;
@@ -24,8 +26,15 @@ int main() {
             cout << "Exiting" << endl;
             break;
         }
-        
-        compiler.compile(filename);
+        else if (filename == "all") {
+            for (const auto& file : directory_iterator(directory_path)) {
+                string filename = file.path().stem().string();
+                compiler.compile(filename);
+            }
+        }
+        else {
+            compiler.compile(filename);
+        }
     }
 }
 
