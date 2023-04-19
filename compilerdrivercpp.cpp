849bccc28a52a20d8818d57cb8c4c54fea086d99
Michael <osujim2012@gmail.com>
2023-04-19 00:07:36 -0400

    Use compiler class

--- compilerdrivercpp.cpp
+++ compilerdrivercpp.cpp
@@ -1,22 +1,31 @@
-#include "Semantic/SemanticAnalyzer.h"
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include "Compiler/Compiler.h"
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::filesystem::directory_iterator;
 
 int main() {
-    std::vector<std::string> test_files{ "syntax-func", "example-bubblesort" }; //"polynomialsemanticerrors", "example-polynomial", ,
-//, "full-coverage" };
+    Compiler compiler;
 
-    for (auto filename : test_files) {
-        std::string source_file = "input/" + filename + ".src";
-        std::cout << filename << std::endl;
-        if (std::filesystem::exists(source_file)) {
-            SemanticAnalyzer sa = SemanticAnalyzer(filename);
-            sa.analyze();
+    while (true) {
+        cout << "Please enter the name of the file (without the extension) you want to compile or type \"exit\" to stop compiling files. The avaiables files in the input directory are:" << endl;
+        string directory_path = "input";
+        for (const auto& file : directory_iterator(directory_path)) {
+            cout << file.path().filename() << endl;
         }
-        else {
-            std::cout << source_file << " doesn't exist. Please make sure the file is in the input folder. Skipping file." << std::endl;
+        string filename;
+        cin >> filename;
+        if (filename == "exit") {
+            cout << "Exiting" << endl;
+            break;
         }
+        
+        compiler.compile(filename);
     }
-    //std::cin.get();
 }
+
